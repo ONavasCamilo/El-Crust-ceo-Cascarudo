c48dd1ef4361e30da0d6c4fbd07b6a6f782d0f54
Aldo Aliscioni <aldoaliscioni18@gmail.com>
2024-04-06 19:50:39 -0300

    init express

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world');
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.get("/ping", (req, res) => {
+  return res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running, port: ${PORT}`);
+});
