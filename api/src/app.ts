f674b47ff92e153df3c2c9fcc9a4fd1cd28364f3
camilo Navas <navasochoa@hotmail.com>
2024-04-06 19:09:11 -0500

    modularizacion express

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import morgan from 'morgan'
+import pingRouter from "./routes/ping.routes";
 
 const app = express();
+
 app.use(express.json());
 app.use(morgan('dev'))
 
-app.get("/ping", (req, res) => {
-    return res.send("pong");
-  });
+app.use("/ping", pingRouter)
 
 export default app
---
+++ api/src/services/ping.services.ts
@@ -0,0 +1,6 @@
+const DB_PING = ["ping"]
+
+export const pingServices = () => {
+    const ping = DB_PING
+    return ping
+}
