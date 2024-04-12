e768fde5788c0763a2f7456bd3e0d34c86eca513
camilo Navas <navasochoa@hotmail.com>
2024-04-12 07:05:56 -0500

    DATABASE_PORT ProductRepository .env

--- api/src/config/data-source.ts
+++ api/src/config/data-source.ts
@@ -1,19 +1,28 @@
 import { DataSource } from "typeorm";
-import { DATABASE_PASSWORD, DATABASE_USERNAME, DATABASE_HOST, DATABASE_NAME } from "./envs";
+import {
+  DATABASE_PASSWORD,
+  DATABASE_USERNAME,
+  DATABASE_HOST,
+  DATABASE_NAME,
+  DATABASE_PORT,
+} from "./envs";
 import { Burger } from "../entities/Burger";
+import { Product } from "../entities/Product";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: DATABASE_HOST,
-    port: 5432,
-    username: DATABASE_USERNAME,
-    password: DATABASE_PASSWORD,
-    database: DATABASE_NAME,
-    synchronize: true,
-    logging: true,
-    entities: [Burger],
-    subscribers: [], 
-    migrations: [],
+  type: "postgres",
+  host: DATABASE_HOST,
+  port: Number(DATABASE_PORT),
+  username: DATABASE_USERNAME,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
+  synchronize: true,
+  logging: true,
+  entities: [Burger, Product],
+  subscribers: [],
+  migrations: [],
+  // dropSchema: true, //! elimina todo de la base de datos
 });
 
-export const BurgerModel = AppDataSource.getRepository(Burger);
+export const BurgerRepository = AppDataSource.getRepository(Burger);
+export const ProductRepository = AppDataSource.getRepository(Product);
