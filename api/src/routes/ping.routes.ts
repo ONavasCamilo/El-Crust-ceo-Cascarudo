363c834213efa3ea6340e57a77965adfc1e344fa
Aldo Aliscioni <aldoaliscioni18@gmail.com>
2024-04-13 19:24:19 -0300

    removed ping

--- api/src/routes/ping.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { pingController } from '../controllers/ping.controller'
-
-const router = Router()
-
-router.get("/", pingController)
-
-export default router
