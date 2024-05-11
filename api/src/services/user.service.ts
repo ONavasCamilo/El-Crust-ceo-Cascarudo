7a13cea723d4ee76ec94e5ad7b34109026ef8af9
camilo Navas <navasochoa@hotmail.com>
2024-05-11 14:52:52 -0500

    users find sin retornar password

--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -1,54 +1,72 @@
 import { UserModel } from "../config/data-source";
 import { hashPassword } from "../utils/passwordManager";
 import RoleModel from "../repositories/rol.repository";
-import { DeleteUser, LoginUser, RegisterUser } from './../interfaces/user.interface';
+import {
+  DeleteUser,
+  LoginUser,
+  RegisterUser,
+} from "./../interfaces/user.interface";
 
 export const getUsersService = async () => {
   const users = await UserModel.find();
   return users;
-}
+};
 
-export const getUserService = async ({ id, username, email }: { id: string | undefined, username: string | undefined, email: string | undefined }) => {
+export const getUserService = async ({
+  id,
+  username,
+  email,
+}: {
+  id: string | undefined;
+  username: string | undefined;
+  email: string | undefined;
+}) => {
   if (id) {
-    return await UserModel.findBy({ id })
+    const user = await UserModel.findBy({ id });
+    return user.map(({ password, ...userWithoutPw }) => userWithoutPw);
+  } else if (username) {
+    const user = await UserModel.findBy({ username });
+    return user.map(({ password, ...userWithoutPw }) => userWithoutPw);
+  } else if (email) {
+    const user = await UserModel.findBy({ email });
+    return user.map(({ password, ...userWithoutPw }) => userWithoutPw);
   }
-  else if (username) {
-    return await UserModel.findBy({ username })
-  }
-  else if (email) {
-    return await UserModel.findBy({ email })
-  }
-}
+};
 
-export const registerUserService = async ({ username, password, email }: RegisterUser) => {
-
+export const registerUserService = async ({
+  username,
+  password,
+  email,
+}: RegisterUser) => {
   const passwordHash = await hashPassword(password);
-  const newUser = UserModel.create({ username, password: passwordHash, email })
-  await RoleModel.asignRole(newUser)
-  await newUser.save()
-  return newUser
-}
+  const newUser = UserModel.create({ username, password: passwordHash, email });
+  await RoleModel.asignRole(newUser);
+  await newUser.save();
+  return newUser;
+};
 
 export const loginUserService = async ({ username, email }: LoginUser) => {
   return username
     ? await UserModel.findOne({ where: { username }, relations: ["role"] })
     : await UserModel.findOne({ where: { email }, relations: ["role"] });
-}
+};
 
-export const deleteUserService = async ({ id, username, email }: DeleteUser) => {
+export const deleteUserService = async ({
+  id,
+  username,
+  email,
+}: DeleteUser) => {
   if (id) {
-    const user = await UserModel.findOne({ where: { id } })
+    const user = await UserModel.findOne({ where: { id } });
     if (!user) throw new Error("User not found");
-    await user?.remove()
-  }
-  else if (username) {
-    const user = await UserModel.findOne({ where: { username } })
+    await user?.remove();
+  } else if (username) {
+    const user = await UserModel.findOne({ where: { username } });
     if (!user) throw new Error("User not found");
-    await user?.remove()
-  }
-  else if (email) {
-    const user = await UserModel.findOne({ where: { email } })
+    await user?.remove();
+  } else if (email) {
+    const user = await UserModel.findOne({ where: { email } });
     if (!user) throw new Error("User not found");
-    await user?.remove()
+    await user?.remove();
   }
-}
+};
